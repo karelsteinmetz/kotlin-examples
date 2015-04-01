d9a76277ad5e3ce3ce63acbf74a50c9da923f82c
Karel Steinmetz <karel.steinmetz@gmail.com>
2015-04-01 16:07:27 +0200

    Added sample for ReifiedTypeParameters.

--- kotlin-samples-m10/src/main/kotlin/org/ks/kotlin/samples/m10/Main.kt
+++ kotlin-samples-m10/src/main/kotlin/org/ks/kotlin/samples/m10/Main.kt
@@ -1,9 +1,6 @@
 package org.ks.kotlin.samples.m10
 
 
-inline fun methodsOf<reified T>() = javaClass<T>().getMethods()
-
 fun main(s: Array<String>) {
-    println("Methods of ${javaClass<String>()}")
-    println(methodsOf<String>().joinToString("\n"))
+    println("Hello world")
 }
