7c1403ca903364daf27df3079e57c64a2479235b
Karlos <karel.steinmetz@gmail.com>
2015-04-01 22:39:28 +0200

    Added sample for data classes.

---
+++ kotlin-samples-base-syntax/src/main/kotlin/org/ks/kotlin/samples/base/syntax/Command.kt
@@ -0,0 +1,27 @@
+package org.ks.kotlin.samples.base.syntax
+
+
+trait Routable {
+    val routingKey: String
+}
+
+trait DtoHolder<out T> {
+    val dto: T
+//    var mutableDto: T
+}
+
+abstract class Command<T>(override var dto: T) : Routable, DtoHolder<T> {
+}
+
+class CreateWorkflowCommnad(var workflow: WorkflowDto? = null): Command<WorkflowDto>(workflow!!) {
+    override val routingKey: String
+        get() = "workflow.command"
+}
+
+data class WorkflowDto(var id: Long,
+                       var modules: Iterable<ModuleDto>,
+                       var connections: Iterable<ConnectionDto>)
+
+data class ModuleDto {}
+
+data class ConnectionDto {}
---
+++ kotlin-samples-base-syntax/src/test/kotlin/org/ks/kotlin/samples/base/syntax/CommandTest.kt
@@ -0,0 +1,44 @@
+package org.ks.kotlin.samples.base.syntax
+
+import org.junit.Test
+import org.ks.kotlinfluentassertions.core.should
+
+public class CommandTest {
+
+    Test
+    fun hasWorkflowDto() {
+        val w = WorkflowDto(1L, listOf<ModuleDto>(), listOf<ConnectionDto>())
+        val command = CreateWorkflowCommnad(w)
+
+        command.dto.should().be(w)
+    }
+
+    Test
+    fun dataClassCanBeCopied() {
+        val w = WorkflowDto(1L, listOf<ModuleDto>(), listOf<ConnectionDto>())
+
+        println(w)
+
+        val w2 = w.copy(w.id, w.modules, w.connections)
+
+        println(w2)
+
+        w.should().be(w2)
+        w.equals(w2).should().be(true)
+        w.equals(w2).should().beTruthy()
+    }
+
+    Test
+    fun dataClassCanBeUsedToMultiDeclarations() {
+        val w = WorkflowDto(1L, listOf<ModuleDto>(), listOf<ConnectionDto>())
+
+        val (id, modules, connections) = w
+
+        id.should().be(1L)
+        modules.count().should().be(0)
+        connections.count().should().be(0)
+
+        val (id2) = w
+        id2.should().be(1L)
+    }
+}
