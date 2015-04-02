7e07c0ef21e04e44200f876a0bbca96a053926d8
Karlos <karel.steinmetz@gmail.com>
2015-04-02 18:30:05 +0200

    Added sample for @labels inside lambdas.

--- kotlin-samples-m11/src/test/kotlin/org/ks/kotlin/samples/LabeledReturnsTest.kt
+++ kotlin-samples-m11/src/test/kotlin/org/ks/kotlin/samples/LabeledReturnsTest.kt
@@ -6,18 +6,45 @@ import org.ks.kotlinfluentassertions.core.should
 public class LabeledReturnsTest {
 
     Test
-    fun mapsValuesWhichAreLessThanZero() {
+    fun labelForMapExpressionIsImplicit() {
         val listOfInts = listOf(1, -10, 100, -1000)
 
         val listOfStrings = listOfInts.map { i ->
             if (i < 0)
-                return@map "Negative"
-            "Positive"
+                return@map -1
+            //...
+            i * 2
         }
 
-        listOfStrings[0].should().be("Positive")
-        listOfStrings[1].should().be("Negative")
-        listOfStrings[2].should().be("Positive")
-        listOfStrings[3].should().be("Negative")
+        listOfStrings[0].should().be(2)
+        listOfStrings[1].should().be(-1)
+        listOfStrings[2].should().be(200)
+        listOfStrings[3].should().be(-1)
+    }
+
+    Test
+    fun canReturnOnOuterFunction() {
+        class Vertex(var neighbors: Iterable<Vertex> = listOf()) {
+        }
+
+        val f =
+                fun reachable(from: Vertex, to: Vertex): Boolean {
+                    val visited = hashSetOf<Vertex>()
+                    fun dfs(current: Vertex) {
+                        // here we return from the outer function:
+                        //if (current == to) return@reachable true
+                        // And here -- from local function:
+                        if (!visited.add(current)) return
+                        for (v in current.neighbors)
+                            dfs(v)
+                    }
+
+                    dfs(from)
+                    return false // if dfs() did not return true already
+                }
+
+        f(Vertex(), Vertex())
+                .should().beFalsy()
+
     }
 }
