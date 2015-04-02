d7a66831bf98d79134aa31f610782caf62f8d820
Karlos <karel.steinmetz@gmail.com>
2015-04-02 11:42:52 +0200

    Added sample for open class.

--- kotlin-samples-base-syntax/src/main/kotlin/org/ks/kotlin/samples/base/syntax/KotlinClass.kt
+++ kotlin-samples-base-syntax/src/main/kotlin/org/ks/kotlin/samples/base/syntax/KotlinClass.kt
@@ -13,9 +13,12 @@ trait DtoHolder<out T> {
 abstract class Command<T>(override var dto: T) : Routable, DtoHolder<T> {
 }
 
-class CreateWorkflowCommand(var workflow: WorkflowDto? = null): Command<WorkflowDto>(workflow!!) {
+open class CreateWorkflowCommand(var workflow: WorkflowDto? = null): Command<WorkflowDto>(workflow!!) {
     override val routingKey: String
         get() = "workflow.command"
+}
+
+class CreateDefaultWorkflowCommand(): CreateWorkflowCommand(WorkflowDto(-1L, listOf(), listOf())) {
 }
 
 internal fun CreateWorkflowCommand.printToConsole(): Unit {
