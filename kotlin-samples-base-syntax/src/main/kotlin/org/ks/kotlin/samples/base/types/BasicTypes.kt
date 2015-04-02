fb7ac9ee6b3ec9718092987a3f06266b78062c48
Karlos <karel.steinmetz@gmail.com>
2015-04-02 18:05:14 +0200

    Added sample for basic types.

---
+++ kotlin-samples-base-syntax/src/main/kotlin/org/ks/kotlin/samples/base/types/BasicTypes.kt
@@ -0,0 +1,15 @@
+package org.ks.kotlin.samples.base.types
+
+public class BasicTypes(
+        var double: Double = 64.0,
+        var float: Float = 32F,
+        var long: Long = 64L,
+        var int: Int = 23,
+        var short: Short = 16,
+        var byte: Byte = 8,
+        var char: Char = 'a',
+        var string: String = "AString",
+        var boolean: Boolean = true,
+        var nullableDouble: Double? = 64.0
+) {
+}
---
+++ kotlin-samples-base-syntax/src/test/kotlin/org/ks/kotlin/samples/base/types/BasicTypesTest.kt
@@ -0,0 +1,53 @@
+package org.ks.kotlin.samples.base.types
+
+import org.junit.Before
+import org.junit.Test
+import org.ks.kotlinfluentassertions.core.should
+import kotlin.properties.Delegates
+
+public class BasicTypesTest {
+
+    private var h: BasicTypes by Delegates.notNull()
+
+    Before
+    fun before() {
+        h = BasicTypes()
+    }
+
+    Test
+    fun stringHasTemplates() {
+        val anotherString = "AnAnotherString"
+
+        h.string.concat(" + $anotherString")
+                .should().be("AString + AnAnotherString")
+    }
+
+    Test
+    fun stringHasLiterals() {
+        val anotherString = "AnAnotherString"
+
+        val s = h.string.concat(
+"""
+$anotherString
+""")
+        println(s)
+        //s.should().be("AString\nAnAnotherString")
+    }
+
+    Test
+    fun numbersHasNotImplicitConversion() {
+        h.float = 1F
+        h.float = 1f
+
+        //        h.Long = h.float
+        //        h.Long = 1l
+    }
+
+    Test
+    fun explicitConversion() {
+        h.long = h.float.toLong()
+
+        h.byte = 1
+        h.long = h.byte.toLong()
+    }
+}
